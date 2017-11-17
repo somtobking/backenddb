7553623133e13cbc48bc6b8d54746105cf027c4c
Somto Okoye <somto@MacBook-Pro-7.local>
2017-11-16 21:39:06 -0600

    routes changed

--- backend/Upstagged/src/routes.php
+++ backend/Upstagged/src/routes.php
@@ -4,22 +4,22 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
-// $app->get('/shows', function ($request, $response, $args)
-// {
-//    $PDO = $this->db;
-//    $query = $PDO->prepare("SELECT * FROM shows");
-//    $PDO->execute();
-//    $shows = $PDO->fetchAll();
-//    return $this-> response->withJson($shows);
+$app->get('/shows', function ($request, $response, $args)
+{
+   $PDO = $this->db;
+   $query = $PDO->prepare("SELECT * FROM shows");
+   $PDO->execute();
+   $shows = $PDO->fetchAll();
+   return $this-> response->withJson($shows);
     
-// });
-
-$app->get('/shows', function ($request, $response, $args) {
-   $sth = $this->db->prepare("SELECT * FROM shows");
-   $sth->execute();
-   $shows = $sth->fetchAll();
-   return $this->response->withJson($shows);
 });
+
+// $app->get('/shows', function ($request, $response, $args) {
+//    $sth = $this->db->prepare("SELECT * FROM shows");
+//    $sth->execute();
+//    $shows = $sth->fetchAll();
+//    return $this->response->withJson($shows);
+// });
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
