41ccb4834fcc1e8ba35b42ff31754e62001ebe96
Somto Okoye <somto@MacBook-Pro-7.local>
2017-11-16 20:45:32 -0600

    routes changed

--- backend/Upstagged/src/settings.php
+++ backend/Upstagged/src/settings.php
@@ -15,5 +15,13 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        //Database Section
+        "db" => [
+            "host" => "18.221.82.70",
+            "dbname" => "upstagged",
+            "user" => "up",
+            "pass" => "abc123"
+            ],
     ],
 ];
