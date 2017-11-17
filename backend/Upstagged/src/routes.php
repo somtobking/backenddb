706d9975d2605900736b7241e3439971d317bfdf
Somto Okoye <somto@MacBook-Pro-7.local>
2017-11-16 21:54:37 -0600

    routes fixed

--- backend/Upstagged/src/routes.php
+++ backend/Upstagged/src/routes.php
@@ -14,13 +14,6 @@ $app->get('/shows', function ($request, $response, $args)
     
 });
 
-// $app->get('/shows', function ($request, $response, $args) {
-//    $sth = $this->db->prepare("SELECT * FROM shows");
-//    $sth->execute();
-//    $shows = $sth->fetchAll();
-//    return $this->response->withJson($shows);
-// });
-
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
