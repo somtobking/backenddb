c5ce365bc0ae747d6d383db52562913de94a7204
Somto Okoye <somto@wrls10-8-2-190.resnet.smu.edu>
2017-11-29 12:26:48 -0600

    new routes

--- upstage/src/routes.php
+++ upstage/src/routes.php
@@ -78,8 +78,8 @@ use Slim\Http\Response;
 // ---------- crew routes ----------
 
 // get all crews
-    $app->get('/shows/crew/[{id]}', function ($request, $response, $args) {
-        $sql = "SELECT * FROM shows, crew WHERE show_id= :id";
+    $app->get('/crew/[{id}]', function ($request, $response, $args) {
+        $sql = "SELECT * FROM crew WHERE show_id= :id";
         $sth = $this->db->prepare($sql);
         $sth->bindParam("id", $args['id']);
         $sth->execute();
